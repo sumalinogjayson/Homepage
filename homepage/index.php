414f2df89bea85afb11a47e5be8082aa8d75447b
Jayson B. Sumalinog <84770124+sumalinogjayson@users.noreply.github.com>
2021-05-28 08:37:56 +0800

    Version 1 Jayson B. Sumalinog

---
+++ homepage/index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+	<link rel="stylesheet" type="text/css" href="main.css">
+	<title>PARALUMAN.mnl Accessories Shop</title>
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class="header">
+				<ul class="ul-list">
+					<li class="li-list"><a href="#" class="nav-href">CONTACT</a></li>
+					<li class="li-list"><a href="#" class="nav-href">ABOUT</a></li>
+					<li class="li-list"><a href="#" class="nav-href">PRODUCT</a></li>
+					<div class="user">
+						<a href="" class="btn">LOGIN</a><hr class="div">
+						<a href="" class="btn">REGISTER</a>
+					</div>	
+				</ul>
+			</div>
+		</nav>
+	</header> 	
+<section class="head">
+	<div >
+			<div class="store-logo">
+			<img src="img/logo.jpg" class="logo">
+			<h1 class="name">PARALUMAN.mnl <br>ACCESSORIES</h1>
+			<button class="buy">BUY NOW!!</button>
+			</div>
+	</div>
+</section>
+<section class="product">
+	<h2>PRODUCT</h2>
+	<div class="item-list">
+		<div class="item list">
+			<div class="img num1"></div>
+			<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</P>
+			<button class="prdctBtn">ADD TO CART</button>
+		</div>
+		<div class="item list">
+			<div class="img num2"></div>
+			<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</P>
+			<button class="prdctBtn">ADD TO CART</button>
+	
+		</div>
+		<div class="item list">
+			<div class="img num3"></div>
+			<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</P>
+			<button class="prdctBtn">ADD TO CART</button>
+			
+		</div>
+
+		<div class="item list">
+			<div class="img num4"></div>
+			<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</P>
+			<button class="prdctBtn">ADD TO CART</button>
+			
+		</div>
+		<div class="item list">
+			<div class="img num5"></div>
+			<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</P>
+			<button class="prdctBtn">ADD TO CART</button>
+		</div>
+		<div class="item list">
+			<div class="img num6"></div>
+			<P>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</P>
+			<button class="prdctBtn">ADD TO CART</button>
+		</div>
+	</div>
+	<button>SEE MORE</button>
+</section>
+<section>	
+	<div class="item">
+			<div class="bs pic1"></div>
+			<h3>NECKLACE</h3>
+		</div>
+		<div class="item">
+			<div class="bs pic2"></div>
+			<h3>RINGS</h3>
+		</div>
+		<div class="item">
+			<div class="bs pic3"></div>
+			<h3>EARRINGS</h3>
+		</div>
+</section>
+
+<section class="About-section">
+	
+	<div class="About">
+		<div class="container">
+			<div class="about-img">
+			<img src="img/5.jpg">
+			</div>
+			<div class="about-img2">
+				<img src="img/6.jpg" >
+			</div>
+		</div>
+		<div>
+			<div class="card">
+				<h2 style="color:white;">ABOUT</h2>
+				<p style="color:white;">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sodales elit id finibus bibendum. Pellentesque ultrices placerat bibendum. Ut varius enim vitae mi gravida, ac viverra diam gravida. Nam molestie nunc dolor, et ornare est aliquam vel. Vivamus sagittis molestie facilisis. Aliquam mi tellus, mollis et lobortis condimentum, fermentum dignissim nisi. Curabitur vitae ultricies magna. Pellentesque blandit in dolor vel dignissim.
+				</p>
+			</div>
+		</div>
+	
+	</div>
+</section>
+
+<footer class="Contact">
+	<div>
+	<h1 style="color: white;">Contact Us</h1>
+<div class="about-list">
+	<div class="footer-list">
+		<h3 style="color: white;">CUSTOMER SERVICES<hr></h3>
+		<ul class="">
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		</ul>
+	</div>
+	<div class="footer-list">
+		<h3 style="color: white;">GET TO KNOW<hr></h3>
+		<ul class="">
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		<li class="about-href"><a href="">COMING SOON</a></li>
+		</ul>
+	</div>
+</div>
+</div>
+</footer>
+</body>
+</html>
